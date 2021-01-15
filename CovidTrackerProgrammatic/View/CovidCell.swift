e8e0d98fdbbf82abb1033ab5287a030a7bc63f84
SebaKrk <s.sciuba@gmail.com>
2021-01-15 07:32:45 +0100

    create and setup constraints for all view in CovidCell

--- CovidTrackerProgrammatic/View/CovidCell.swift
+++ CovidTrackerProgrammatic/View/CovidCell.swift
@@ -30,6 +30,69 @@ class CovidCell : UITableViewCell {
         return label
     }()
     
+    let continentLabel : UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textColor = .white
+        label.backgroundColor = .systemBlue
+        label.layer.cornerRadius = 5
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    let casesLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let deathLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let activeLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let testLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = .systemGray
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let todayLabel : UILabel = {
+        let label = UILabel()
+        label.text = "TODAY"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.backgroundColor = .systemRed
+        label.layer.cornerRadius = 5
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    let todayCasesLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let todatDeathLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
     //    MARK: INIT
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -64,6 +127,46 @@ class CovidCell : UITableViewCell {
         countryLabel.translatesAutoresizingMaskIntoConstraints = false
         countryLabel.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
         countryLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 5).isActive = true
+        
+        containerView.addSubview(continentLabel)
+        continentLabel.translatesAutoresizingMaskIntoConstraints = false
+        continentLabel.topAnchor.constraint(equalTo: countryLabel.bottomAnchor, constant: 5).isActive = true
+        continentLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        containerView.addSubview(casesLabel)
+        casesLabel.translatesAutoresizingMaskIntoConstraints = false
+        casesLabel.topAnchor.constraint(equalTo: continentLabel.bottomAnchor).isActive = true
+        casesLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        containerView.addSubview(deathLabel)
+        deathLabel.translatesAutoresizingMaskIntoConstraints = false
+        deathLabel.topAnchor.constraint(equalTo: casesLabel.bottomAnchor).isActive = true
+        deathLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        containerView.addSubview(activeLabel)
+        activeLabel.translatesAutoresizingMaskIntoConstraints = false
+        activeLabel.topAnchor.constraint(equalTo: deathLabel.bottomAnchor).isActive = true
+        activeLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        containerView.addSubview(testLabel)
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        testLabel.topAnchor.constraint(equalTo: activeLabel.bottomAnchor).isActive = true
+        testLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        
+        containerView.addSubview(todayLabel)
+        todayLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayLabel.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        todayLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+
+        containerView.addSubview(todayCasesLabel)
+        todayCasesLabel.translatesAutoresizingMaskIntoConstraints = false
+        todayCasesLabel.topAnchor.constraint(equalTo: todayLabel.bottomAnchor).isActive = true
+        todayCasesLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+
+        containerView.addSubview(todatDeathLabel)
+        todatDeathLabel.translatesAutoresizingMaskIntoConstraints = false
+        todatDeathLabel.topAnchor.constraint(equalTo: todayCasesLabel.bottomAnchor).isActive = true
+        todatDeathLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
         
     }
 }
